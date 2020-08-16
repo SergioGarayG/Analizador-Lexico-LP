8fda7418d645d8f1a0730db87a2fd88326d439d5
julissa239 <62450975+julissa239@users.noreply.github.com>
2020-08-16 02:13:43 -0600

    Rename AnaizadorLexico.cpp to AnalizadorLexico.cpp

--- AnalizadorLexico.cpp
+++ AnalizadorLexico.cpp
@@ -10,7 +10,7 @@ void buscarRESERVADOSID();//FUNCION ENCARGADA DE BUSCAR PALABRAS RESERVADAS EN N
 void Color(int Background,int Text);//FUNCION ENCARGADA DE CAMBIO DE COLORES EN PANTALLA.
 
 const char *RESERVADOSID[] = {"si","entero","inicio","sino","finsi","repetir","hasta","mientras","leer","imprimir","vacio","cursor","definir","hacer"};
-int tamIndicador=sizeof(RESERVADOSID)/sizeof(char *);//Indica el tamaño de nuestras palabras reservadas y el tamaño de memoria de el char
+int tamIndicador=sizeof(RESERVADOSID)/sizeof(char *);//Indica el tamaÃ±o de nuestras palabras reservadas y el tamaÃ±o de memoria de el char
 int i;
 char identificador[150];
 char temp[2];
